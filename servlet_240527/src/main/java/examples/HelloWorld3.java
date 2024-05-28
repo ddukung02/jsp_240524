3d89f4f2a51400ed93180aaa660050c7ae73a246
ddukung02 <yooiwn@gmail.com>
2024-05-28 13:48:38 +0900

    first commit

---
+++ servlet_240527/src/main/java/examples/HelloWorld3.java
@@ -0,0 +1,42 @@
+package examples;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/HelloWorld3")
+public class HelloWorld3 extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		text/html : text/html 내용으로 응답을 함
+//		response.setContentType("text/html");
+//		charset=euc-kr : 한글처리
+//		response.setContentType("text/html;charset=euc-kr");
+//		response.setContentType("text/html;charset=EUC-KR");
+//		response.setContentType("text/html;charset=UTF-8");
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>");
+		out.println("Hello2~");
+		out.println("</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<h1>안녕하세요~!!!</h1>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+}
+
+
+
+
+
+
+
+
